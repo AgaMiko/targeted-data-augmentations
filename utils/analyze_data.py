c5ee38111cd020c9528c2340a612af3a6d69ce01
Agnieszka Mikołajczyk <midnight.inn@gmail.com>
2022-10-21 13:36:00 +0200

    add stats to cbi_analyze_data.py (#6)
    * add stats toanalyze_data.py

--- utils/analyze_data.py
+++ utils/analyze_data.py
@@ -13,8 +13,17 @@ p_aug_type = cbi_results.groupby(by=["aug_type", "p" ]).mean()
 # which mask is the worst
 worst_mask = cbi_results.groupby(by=["aug_type",  "mask_nr"]).mean()
 
+# standard deviation
+std = cbi_results.groupby(by=["model","aug_type", "p"]).std()
+
+# descriptive stats 
+stats = cbi_results.groupby(by=["model","aug_type", "p"]).describe()
+
+
 with pd.ExcelWriter('aggregated_cbi_results.xlsx') as writer:  
     general_aggregated.to_excel(writer, sheet_name='path and aug_type')
     aug_type.to_excel(writer, sheet_name='aug_type')
     p_aug_type.to_excel(writer, sheet_name='p_aug_type')
     worst_mask.to_excel(writer, sheet_name='worst_mask')
+    std.to_excel(writer, sheet_name='std')
+    stats.to_excel(writer, sheet_name='descriptive stats')
